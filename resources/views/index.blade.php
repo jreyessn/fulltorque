c71cdd0711dc56467022945a4de5d7998703cc98
JuanR <snjuank@gmail.com>
2023-09-24 11:04:34 -0400

    feature: Livewire instalacion

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,6 +20,8 @@
         <link href="{{ asset('assets/plugins/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
         <link href="{{ asset('assets/plugins/datatables/buttons.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
         <link href="{{ asset('assets/plugins/datatables/responsive.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
+
+        @livewireStyles
     </head>
     <body>
         <div id="page" class="d-flex flex-column">
@@ -48,7 +50,9 @@
         <!-- Responsive examples -->
         <script src="{{ asset("assets/plugins/datatables/dataTables.responsive.min.js") }}"></script>
         <script src="{{ asset("assets/plugins/datatables/responsive.bootstrap4.min.js") }}"></script>
-         <script src="{{ asset("assets/plugins/sweet-alert2/sweetalert2.min.js") }}"></script>
-     
+        <script src="{{ asset("assets/plugins/sweet-alert2/sweetalert2.min.js") }}"></script>
+        
+        @livewireScripts
+
     </body>
 </html>
